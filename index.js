07f875d494d17da88cb46ce696dd1072fd71a376
Ayushi Shah <ayushishah509@gmail.com>
2025-08-04 16:01:32 +0530

    gitignore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { ApolloServer } = require("apollo-server");
-const { typeDefs } = require("./graphQL/schema/typedef");
-const { resolvers } = require("./graphQL/schema/resolvers");
+const { typeDefs } = require("./schema/typedef");
+const { resolvers } = require("./schema/resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
